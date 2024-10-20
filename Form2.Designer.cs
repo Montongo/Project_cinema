1b7b791cb3270dfce04c2544248ffb2f8d40915a
Jorg3sss <128569375+Jorg3sss@users.noreply.github.com>
2024-10-20 12:10:24 -0600

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Proyecto_cine
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            linkLabel1 = new LinkLabel();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 0, 192);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.ForeColor = Color.Snow;
+            panel1.Location = new Point(-3, -1);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(806, 452);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkBlue;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(803, 50);
+            panel2.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(366, 226);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(366, 162);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "  ";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.FromArgb(224, 224, 224);
+            linkLabel1.Location = new Point(314, 329);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(177, 20);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "¿Olvidaste la contraseña?";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(236, 226);
+            label3.Name = "label3";
+            label3.Size = new Size(124, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Contraseña: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(245, 162);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario: ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Franklin Gothic Medium Cond", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(314, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(148, 36);
+            label1.TabIndex = 0;
+            label1.Text = "¡Bienvenido!";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(468, 81);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(306, 277);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private LinkLabel linkLabel1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
